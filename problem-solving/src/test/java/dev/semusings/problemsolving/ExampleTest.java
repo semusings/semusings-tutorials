c7fcfeb08d50c106a4ad16b026522179b1959720
semusings <bhuwan.upadhyay49@gmail.com>
2024-11-15 22:11:02 -0600

    add problem solving maven project

---
+++ problem-solving/src/test/java/dev/semusings/problemsolving/ExampleTest.java
@@ -0,0 +1,35 @@
+package dev.semusings.problemsolving;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ExampleTest {
+
+// @formatter:off
+
+  private static Stream<Arguments> provider() {
+    return Stream.of(
+        Arguments.of("aab", "aba"),
+        Arguments.of("aaab", "")
+    );
+  }
+//@formatter:on
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void test(String s, String expected) {
+        Assertions.assertEquals(expected, new Solution1().solve(s));
+    }
+
+
+    private static class Solution1 {
+
+        public String solve(String s) {
+            return null;
+        }
+    }
+}
